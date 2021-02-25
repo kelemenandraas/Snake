4f23189c568773f537497ea608f32eeedc10aa9e
kelemenandraas <kelemenandraas0219@gmail.com>
2021-02-25 09:52:29 +0100

    A fej létrehozása és előre mozgatása.

---
+++ snake.py
@@ -0,0 +1,18 @@
+import turtle
+import time
+
+palya = turtle.Screen()
+palya.setup(width=800, height=600)
+palya.bgcolor("green")
+palya.title("Snake")
+palya.tracer(0)
+
+fej = turtle.Turtle()
+fej.shape("triangle")
+fej.penup()
+fej.color("yellow")
+
+while True:
+    fej.forward(20)
+    palya.update()
+    time.sleep(0.3)
